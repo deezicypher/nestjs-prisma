04433cbefd02f690db668c5df47e1c6ac58118c8
Deezi <amuhdavid@gmail.com>
2025-11-21 12:50:24 +0100

    users and profile

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { SongsModule } from './songs/songs.module';
 import { ArtistsModule } from './artists/artists.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [SongsModule, ArtistsModule],
+  imports: [SongsModule, ArtistsModule, UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
